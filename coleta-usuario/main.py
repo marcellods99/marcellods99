2b3304fe40ccefd5614e42ed55b8f718fb38fc4a
Marcello Dias Silveira <59810968+marcellods99@users.noreply.github.com>
2026-06-07 11:35:18 -0300

    Limpa projetos antigos do perfil

--- coleta-usuario/main.py
+++
@@ -1,9 +0,0 @@
-"""
-Questão 3
-Crie um programa que solicite à pessoa usuária digitar seu nome, idade e altura em metros, e imprima “Olá, [nome], você tem [idade] anos e mede [altura] metros!”.
-"""
-
-nome = str(input('Digite o seu nome:\n'))
-idade = int(input('Digite sua idade:\n'))
-altura = float(input('Digite sua altura:\n'))
-print(f'Olá {nome}, você tem {idade} e mede {altura} metros!')
