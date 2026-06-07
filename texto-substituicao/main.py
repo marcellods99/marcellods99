68beeb94899d6dbd5062127adbafc73971b866e5
Marcello Dias Silveira <59810968+marcellods99@users.noreply.github.com>
2026-06-07 11:34:10 -0300

    Atualiza projetos em destaque

--- texto-substituicao/main.py
+++
@@ -1,7 +0,0 @@
-"""
-Questão 9
-Crie um código que solicite uma frase à pessoa usuária e imprima a mesma frase com todas as vogais “a” trocadas pela caractere “@”.
-"""
-
-frase = input('Digite uma frase: ')
-print(frase.replace("a","@"))
